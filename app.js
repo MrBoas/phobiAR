aa2878e2256ca67304209baba7a8d2df7b7cda70
raulvilasboas97 <raulvilasboas97@gmail.com>
2020-03-29 13:54:05 +0100

    change models to public (not best solution) & render html with pug

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 var createError = require('http-errors');
 const app = express();
+var path = require('path');
 app.set('view engine', 'pug');
+
+// app.set('view engine', 'html');
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -21,15 +28,30 @@ mongoose
         console.log('DB Connection Error: ${ err.message }');
     });
 
-const server = app.listen(3000, () => {
+app.listen(3000, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function (req, res) {
+    // console.log(__dirname)
+    // res.sendFile(path.join(__dirname + '/files/testing.html'));
+    res.sendFile('files/testing.html',{root:__dirname});
+    // res.render('testing')
+});
+
+app.get('/pug', function(req,res){
+    res.render('testing')
+})
 
 // Routing
 app.use('/api/phobias', require('./routes/api/phobias'));
 app.use('/api/markers', require('./routes/api/markers'));
 app.use('/api/sessions', require('./routes/api/sessions'));
+
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
